855a841fec7e0fa9b143ddbb9e732c65aebc2558
anjanaraveendra <anjana@cognitiveclouds.com>
2015-05-06 10:32:10 +0530

    CRUD method implemented

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -10,6 +10,11 @@ class JobsController < ApplicationController
     @jobs = Job.page(params[:page]).per(25)
   end
 
+  def show
+    @job = Job.find(params[:id])
+    render :action => 'show'
+  end
+
   def create
     @job = Job.new(params.require(:job).permit(:title, :company, :url))
   if @job.save
@@ -18,4 +23,42 @@ class JobsController < ApplicationController
     render "new"
   end
  end
+
+ def edit
+    @job = Job.find(params[:id])
+ end
+
+# def update
+#     @job = Job.find(params[:id])
+#     if @job.update(job_params)
+#       format.html { redirect_to @job, notice: 'Job was successfully updated.' }
+#       format.json { head :no_content }
+#     else
+#       render 'edit'
+#     end
+# end
+
+  def update
+    @job = Job.find params[:id]
+
+      if @job.update(job_params)
+        redirect_to jobs_path      
+      else
+        format.html { render :edit }
+      end
+  end
+
+ def destroy
+    @job = Job.find(params[:id])
+    @job.destroy
+    redirect_to(:back)
+    flash[:error] = "Job was destroyed!"
+  end
+
+  private
+
+   def job_params
+     params.require(:job).permit(:title, :company, :url)
+   end
+
 end
